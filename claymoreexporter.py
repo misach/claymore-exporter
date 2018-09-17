18660083bc39cc156b5385e0c315b060dd414ebf
misach <43113243+misach@users.noreply.github.com>
2018-09-17 21:40:00 +0200

    Added support for miner_getstat2 jsonrpc

--- claymoreexporter.py
+++ claymoreexporter.py
@@ -27,6 +27,13 @@ REQUEST_GPU_TEMP  = Gauge('claymore_gpu_temp','Claymore GPU temp', ['gpu_id'])
 REQUEST_GPU_FAN  = Gauge('claymore_gpu_fan','Claymore GPU fan', ['gpu_id'])
 REQUEST_GPU_HR1  = Gauge('claymore_gpu_hashrate_1','Claymore GPU hashrate1', ['gpu_id'])
 REQUEST_GPU_HR2  = Gauge('claymore_gpu_hashrate_2','Claymore GPU hashrate2', ['gpu_id'])
+REQUEST_GPU_PCI = Gauge('claymore_gpu_pci','Claymore GPU pci slot', ['gpu_id'])
+REQUEST_GPU_ETH_SHARES_ACCEPTED = Gauge('claymore_gpu_eth_shares_accepted','Claymore GPU ETH accepted shares', ['gpu_id'])
+REQUEST_GPU_ETH_SHARES_REJECTED = Gauge('claymore_gpu_eth_shares_rejected','Claymore GPU ETH rejected shares', ['gpu_id'])
+REQUEST_GPU_ETH_SHARES_INVALIDE = Gauge('claymore_gpu_eth_shares_invalide','Claymore GPU ETH invalide shares', ['gpu_id'])
+
+REQUEST_INFO        =  Gauge('claymore_info', 'Claymore informations',['version','dcr_mining_pool','eth_mining_pool'])
+REQUEST_RUNNING_TIME =  Gauge('claymore_running_time','Claymore Miner Running Time')
 
 REQUEST_COIN1_SHARES = Counter('claymore_coin1_shares','Claymore coin1 share')
 REQUEST_COIN1_REJECT = Counter('claymore_coin1_shares_reject','Claymore coin1 share reject')
@@ -40,8 +47,10 @@ if __name__ == "__main__":
     # Main loop
     while True:
 
-        data = netcat(ip, port, '{"id":0,"jsonrpc":"2.0","method":"miner_getstat1"}' )
+        # Getting Miner Data with json rpc minder_getstat2 API0
+        data = netcat(ip, port, '{"id":0,"jsonrpc":"2.0","method":"miner_getstat2"}' )
 
+        # print data
         # Get Claymore version and running time from raw data
         received_data['claymore_version'] = data['result'][0]
         received_data['running_time'] = data['result'][1]
@@ -67,6 +76,8 @@ if __name__ == "__main__":
                 received_data['gpu'][i]['hashrate2'] = 0
                 received_data['gpu'][i]['temp'] = 0
                 received_data['gpu'][i]['fan']  = 0
+
+        REQUEST_RUNNING_TIME.set(received_data['running_time'])
 
         # Get total hash rate from Claymore raw data
         total_coin_array = data['result'][2].split(';')
@@ -145,6 +156,46 @@ if __name__ == "__main__":
             received_data['gpu'][id]['temp'] = tf[i*2]
             received_data['gpu'][id]['fan']  = tf[(i*2)+1]
             id+=1
+        
+        # Mining Pool URL
+        poolurl = data['result'][7].split(';')
+        
+        try:
+            dcr_pool_url = poolurl[1]
+        except Exception as e :
+            dcr_pool_url = "off"
+        
+        eth_pool_url = poolurl[0]
+        REQUEST_INFO.labels(received_data['claymore_version'],dcr_pool_url, eth_pool_url).set(1)
+
+        # ETH accepted shares for every GPU.
+        id = 0
+        for i in data['result'][9].split(';'):
+            received_data['gpu'][id]['eth_shares_accepted'] = i
+            id+=1
+
+        # ETH rejected shares for every GPU.
+        id = 0
+        for i in data['result'][10].split(';'):
+            received_data['gpu'][id]['eth_shares_rejected'] = i
+            id+=1
+        
+        # ETH invalid shares for every GPU.
+        id = 0
+        for i in data['result'][11].split(';'):
+            received_data['gpu'][id]['eth_shares_invalide'] = i
+            id+=1
+        # DCR accepted shares for every GPU.
+        # DCR rejected shares for every GPU.
+        # DCR invalid shares for every GPU.
+
+
+        # PCI bus index for every GPU.
+        pci_idx = data['result'][15].split(';')
+        
+        #print pci
+        for i in range (0,len(received_data['gpu'])):
+            received_data['gpu'][i]['pci_bus_index'] = pci_idx[i]
 
         print received_data
 
@@ -153,6 +204,10 @@ if __name__ == "__main__":
             REQUEST_GPU_FAN.labels(i).set(received_data['gpu'][i]['fan'])
             REQUEST_GPU_HR1.labels(i).set(received_data['gpu'][i]['hashrate1'])
             REQUEST_GPU_HR2.labels(i).set(received_data['gpu'][i]['hashrate2'])
+            REQUEST_GPU_ETH_SHARES_ACCEPTED.labels(i).set(received_data['gpu'][i]['eth_shares_accepted'])
+            REQUEST_GPU_ETH_SHARES_REJECTED.labels(i).set(received_data['gpu'][i]['eth_shares_rejected'])
+            REQUEST_GPU_ETH_SHARES_INVALIDE.labels(i).set(received_data['gpu'][i]['eth_shares_invalide'])
+            REQUEST_GPU_PCI.labels(i).set(received_data['gpu'][i]['pci_bus_index'])
 
         time.sleep(sleep_time)
 
